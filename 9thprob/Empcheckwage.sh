8ffa84e08d36ef2a6d5b3b966afa63f40a74bfa0
Vallem Balu <vbnreddy5@gmail.com>
2021-06-13 20:07:23 +0530

    add emphrpart

--- 9thprob/Empcheckwage.sh
+++ 9thprob/Empcheckwage.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -x
 isFullTime=1
+isPartTime=2
 empRateperHr=20
 empFullTime=8
-randomCheck=$((RANDOM%2))
+randomCheck=$((RANDOM%3))
 if [ $isFullTime -eq $randomCheck ]
 then
         echo "Employee is Present"
+	empHr=8
+	echo $empHr hr
+elif [ $isFPartTime -eq $randomCheck ]
+then
+	echo "Employee is parttime"
+	empHr=4
+	echo $empHr hr
 else
         echo "Employee is absent"
 fi
