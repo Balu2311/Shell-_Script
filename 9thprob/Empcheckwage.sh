907d647d0c81d9ff5804d258bd5f53fee42af8a0
Vallem Balu <vbnreddy5@gmail.com>
2021-06-13 20:16:56 +0530

    add caseStatement

--- 9thprob/Empcheckwage.sh
+++ 9thprob/Empcheckwage.sh
@@ -4,16 +4,18 @@ isPartTime=2
 empRateperHr=20
 empFullTime=8
 randomCheck=$((RANDOM%3))
-if [ $isFullTime -eq $randomCheck ]
-then
-        echo "Employee is Present"
-	empHr=8
-	echo $empHr hr
-elif [ $isFPartTime -eq $randomCheck ]
-then
-	echo "Employee is parttime"
-	empHr=4
-	echo $empHr hr
-else
-        echo "Employee is absent"
-fi
+case $randomCheck in
+	$isFullTime)
+		echo "Employee is Present"
+		empHr=8
+		echo $empHr hr
+		;;
+	$isFPartTime)
+		echo "Employee is parttime"
+		empHr=4
+		echo $empHr hr
+		;;
+	0)
+        	echo "Employee is absent"
+		;;
+esac
